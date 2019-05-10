4a431588e5ff46feb57e3b3efac278a9c9a6b714
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-05-10 14:29:53 +0100

    wrote jquery code to render first 12 articles with working picture links and place them in responsive grid container. add on hover functionality to aalso show abstract on hover.

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 
-$( document ).ready(function() {
+$(document).ready(function() {
 $('#newsSection').change(function(){
-    var value = $(this).val();
-    $.getJSON("https://api.nytimes.com/svc/topstories/v2/" +value+".json?api-key=JZGkmnwVvjwt6MH5Fh2i4dL1nQTF2AD9").done(function(articles) {
-        let articleBox = $('.container').append("<div></div>")
-        $(articleBox).append("<p>"+articles.results[0].title+"</p>")
-        $(articleBox).attr("background-image", +articles.results[0].multimedia[1].url )
+    var section = $(this).val();
+    // Show loading icon
+    $(".loading-indicator").show();
+    $(".loadingcontainer").show();
+    $(".articlesContainer").empty()
+    $.getJSON("https://api.nytimes.com/svc/topstories/v2/" +section+".json?api-key=JZGkmnwVvjwt6MH5Fh2i4dL1nQTF2AD9").done(function(topStories) {
+        $(".header").css("flex-direction", "row")
+        $(".header").css("justify-content", "flex-start")
+        $(".header").css("height", "150px")
+        $(".header").css("margin", "30px")
 
+        $.each(topStories.results, function () {
+          //End at this point
+          if (document.querySelector(".articlesContainer").childElementCount == 12) {
+            return false;
+          }
+          else {
+              //If the article object contains an image url
+            if (this.multimedia[4] !== undefined){
+
+              $( "<a class='newArticle'></a>" ).appendTo( ".articlesContainer" )
+              $('.newArticle').append("<h1>"+this.title+"</h1>")
+              $('.newArticle').append("<div><p>"+this.abstract+"</p></div>")
+              $('.newArticle').css("background-image", "url(" + this.multimedia[4].url + ")")
+              $('.newArticle').attr("href", this.url)
+              $('.newArticle').attr("class", "articleBox")
+            }
+          }
+        })
+      // Hide loading icon
+      $(".loading-indicator").hide()
+      $(".loadingcontainer").hide()
 
       });
+
     });
 })
 
