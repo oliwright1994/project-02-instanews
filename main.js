f964ca590070435610c7ae4a6c7be3ce04e7e0ef
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-05-10 19:38:16 +0100

    added sick animation for moving elements when loaded

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 
 $(document).ready(function() {
-$('#newsSection').change(function(){
+  $('#newsSection').change(function(){
     var section = $(this).val();
     // Show loading icon
+    $(".error").remove();
     $(".loading-indicator").show();
     $(".loadingcontainer").show();
     $(".articlesContainer").empty()
 
 
     $.ajax({
-      url: "https://api.nytimes.com/svc/topstories/v2/" +section+".json?api-key=JZGkmnwVvjwt6MH5Fh2i4dL1nQTF2AD9",
+      url: "https://api.nytimes.com/svc/topstories/v2/" + section + ".json?api-key=JZGkmnwVvjwt6MH5Fh2i4dL1nQTF2AD9",
       type: 'GET',
       datatype: 'json'
     })
-      .fail(function () {
-        $(".loading-indicator").hide()
-        $(".header").css("flex-direction", "row")
-        $(".header").css("justify-content", "flex-start")
-        $(".header").css("height", "150px")
-        $(".header").css("margin", "30px")
-        $(".loadingcontainer").append("<h2>We're sorry but the New York Times API could not be reached at this time</h2>")
-        })
+    .fail(function () {
+      $(".loading-indicator").hide()
+      // $(".header").css("flex-direction", "row")
+      // $(".header").css("justify-content", "flex-start")
+      // $(".header").css("height", "150px")
+      // $(".header").css("margin", "30px")
+      $(".loadingcontainer").append("<h2 class='error'>We're sorry but the New York Times API could not be reached at this time</h2>")
+    })
 
-      .done(function(topStories) {
-        $(".header").css("flex-direction", "row")
-        $(".header").css("justify-content", "flex-start")
-        $(".header").css("height", "150px")
-        $(".header").css("margin", "30px")
+    .done(function(topStories) {
+      $(".articlesContainer").hide()
+      if (window.screen.width >= 1200){
+      $(".header").animate({'height':'150px','margin':'30px'}, 500 )
+      }
+      $(".header").css("flex-direction", "row")
+      $(".header").css("justify-content", "flex-start")
+
 
         $.each(topStories.results, function () {
           //End at this point
-          if (document.querySelector(".articlesContainer").childElementCount == 12) {
+          if (document.querySelector(".articlesContainer").childElementCount === 12) {
             return false;
           }
           else {
-              //If the article object contains an image url
-            if (this.multimedia[4] !== undefined){
+              //Renders the article if all of the 4 values used are defined
+            if (this.multimedia[4] !== undefined && this.title !== undefined && this.abstract !== undefined && this.url !== undefined){
 
               $( "<a class='newArticle'></a>" ).appendTo( ".articlesContainer" )
               $('.newArticle').append("<h1>"+this.title+"</h1>")
@@ -49,7 +53,9 @@ $('#newsSection').change(function(){
       // Hide loading icon
       $(".loading-indicator").hide()
       $(".loadingcontainer").hide()
+      $(".articlesContainer").fadeIn(1000)
       });
     });
 })
+
 
